4354a9e1fb01a357a5ad5e45b991ce01a5f79cec
alevarsi <varsialessandro@gmail.com>
2024-11-11 17:41:25 +0100

    changes

--- TurnOnCurve.cxx
+++ TurnOnCurve.cxx
@@ -1,25 +1,36 @@
 #include <TFile.h>
 #include <TTree.h>
 #include <iostream>
+#include <TEfficiency.h>
 #include <cmath>
 
 void TurnOnCurve() {
     
     TFile* inputFile = new TFile ("merged_2.root", "R");
+
+    if (!inputFile || inputFile->IsZombie()) {
+        std::cerr << "Can't open file" << std::endl;
+        return;
+    }
         
-    Float_t energy_EB_prompt[61200];
-    Float_t energy_EB_hlt[61200];
-    Float_t energy_EB_model[61200];
-    Float_t energy_EB_model_hlt[61200];
+    Float_t energy_EB_prompt;
+    Float_t energy_EB_hlt;
+    Float_t energy_EB_model;
+    Float_t energy_EB_model_hlt;
     
     TTree *tree = (TTree*)inputFile->Get("mergedTree");
     
-    tree->SetBranchAddress("energy_EB_prompt", energy_EB_prompt);
-    tree->SetBranchAddress("energy_EB_hlt", energy_EB_hlt);
-    tree->SetBranchAddress("energy_EB_model", energy_EB_model);
-    tree->SetBranchAddress("energy_EB_model_hlt", energy_EB_model_hlt);
+    tree->SetBranchAddress("energy_EB_prompt", &energy_EB_prompt);
+    tree->SetBranchAddress("energy_EB_hlt", &energy_EB_hlt);
+    tree->SetBranchAddress("energy_EB_model", &energy_EB_model);
+    tree->SetBranchAddress("energy_EB_model_hlt", &energy_EB_model_hlt);
     
-    TH1F* h_turn_on = new TH1F ("h_turn_on", "", 100, 0, 10); //turn on curve
+    float h_min = 1, h_max = 3;
+    int n_bins = 100;
+    TH1F* h_num_model = new  TH1F ("h_num_model", "", n_bins, h_min, h_max); 
+    TH1F* h_den_model = new  TH1F ("h_den_model", "", n_bins, h_min, h_max); 
+    TH1F* h_num_hlt = new  TH1F ("h_num_hlt", "", n_bins, h_min, h_max); 
+    TH1F* h_den_hlt = new  TH1F ("h_den_hlt", "", n_bins, h_min, h_max); 
     
     int nEntries = tree->GetEntries();
     Float_t E_th = 2; // Energy Treshold in GeV
@@ -27,23 +38,39 @@ void TurnOnCurve() {
     for(int iEntry = 0; iEntry < nEntries; iEntry++) {
         
         tree->GetEntry(iEntry);
+        if(energy_EB_hlt >= E_th)
+             h_num_hlt->Fill(energy_EB_prompt);
 
-        for(int ixtal = 0; ixtal < 61200; ixtal++){
-            if(energy_EB_model_hlt[ixtal] >= E_th){
-                
-                h_turn_on->Fill(energy_EB_prompt[ixtal]);
-                
-            }
-            
-        }
-        
+        if(energy_EB_model_hlt >= E_th) 
+            h_num_model->Fill(energy_EB_prompt); 
+
+        h_den_model->Fill(energy_EB_prompt);
+        h_den_hlt->Fill(energy_EB_prompt);
     }
-    
-    inputFile->Close();
-    delete inputFile;
-    TCanvas *c = new TCanvas("c","c",2560,1664);
-    h_turn_on->Draw("");
-    c->Draw();
+
+    //TH1F* h_turn_on_model = new TH1F ("h_turn_on_model", "", n_bins, h_min, h_max); 
+    //TH1F* h_turn_hlt = new TH1F ("h_turn_on_hlt", "", n_bins, h_min, h_max); 
+
+    TEfficiency * turn_on_model_hlt = new TEfficiency(*h_num_model, *h_den_model);
+    turn_on_model_hlt->SetMarkerSize(2);
+    turn_on_model_hlt->SetMarkerColor(kBlue);
+    turn_on_model_hlt->SetLineColor(kBlue);
+
+    TEfficiency * turn_on_hlt = new TEfficiency(*h_num_hlt, *h_den_hlt);
+    turn_on_hlt->SetMarkerSize(2);
+    turn_on_hlt->SetMarkerColor(kRed);
+    turn_on_hlt->SetLineColor(kRed);
+
+    TCanvas *c = new TCanvas("c");
+    //turn_on_model->Draw("AP");
+    turn_on_hlt->Draw("AP");
+    //turn_on_model_hlt->Draw("P same");
+    gPad->Update();
+    turn_on_hlt->GetPaintedGraph()->GetXaxis()->SetTitle("Energy [GeV]");
+    turn_on_hlt->GetPaintedGraph()->GetYaxis()->SetTitle("#epsilon"); 
+    //turn_on_model_hlt->GetPaintedGraph()->GetXaxis()->SetTitle("Energy [GeV]");
+    //turn_on_model_hlt->GetPaintedGraph()->GetYaxis()->SetTitle("#epsilon");     
+
     c->Print("TurnOnCurve_model_hlt.png", "png");
 
 }
